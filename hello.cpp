3fecee9aa80adab0e014d172dd440f04b486aee3
Ethan Gray <90401598+Egray137@users.noreply.github.com>
2022-09-12 08:42:37 -0600

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello, world!" << endl;
+	cout << "My name is Ethan." << endl;
+	cout << "Pleased to meet you." << endl;
+	return 0;
+
+
+}
